34e512e688fc4e389a2aa248cfb18acc9583e29f
서진혁 <seoji@seojinhyeog-ui-MacBookPro.local>
2019-09-01 05:50:19 +0900

    clean push

--- src/pages/CampaignDetailPage.js
+++ src/pages/CampaignDetailPage.js
@@ -7,6 +7,11 @@ const CampaignDetailPage = () => (
     count="14,320"
     desc="일본 상품 불매운동은 일본의 일방적인 무역제재에 항의하여 중국, 한국, 베트남 국민들이 자발적으로 참여하는 일본 제품 불매 운동이다. 일본의 무역제재는 ‘일제 징용배상’ 대법 판결에 불만을 나타내는 것으로 보는 시각이 유력하다"
     bg="http://www.sjsori.com/news/photo/201907/38729_45493_1042.jpg"
+    talkCount="26"
+    brandCount="16"
+    dalor="6"
+    dailyCount="161"
+    notiCount="18"
   />
 );
 
